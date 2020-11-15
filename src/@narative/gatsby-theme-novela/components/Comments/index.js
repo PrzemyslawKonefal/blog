58657ceb97edf79dcc361a111a0ef849601fff22
PrzemyslawKonefal <przemyslaw.konefal@gmail.com>
2020-11-15 18:41:58 +0100

    change disqus reaction embedded text

--- src/@narative/gatsby-theme-novela/components/Comments/index.js
+++ src/@narative/gatsby-theme-novela/components/Comments/index.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Disqus } from 'gatsby-plugin-disqus'
 import styled from "@emotion/styled";
 import mediaqueries from "@narative/gatsby-theme-novela/src/styles/media";
 
+const globalWindow = typeof window !== 'undefined' && window
+
 const Comments = () => {
+    const changeReactionsText = () => {
+        setTimeout(() => {
+            if(globalWindow) {
+                const el = globalWindow.querySelector('#reactions .text-bold')
+                el.innerHTML = 'Czy podobał Ci się ten artykuł?'
+            }
+        }, 2000)
+    }
+
+    useEffect(changeReactionsText, [])
+
     return (
         <Wrapper>
             <Disqus  />
